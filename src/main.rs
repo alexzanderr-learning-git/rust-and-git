fa4340b6c834e6812df552a36d8df8f9471e40ee
alexzanderr <alexxander18360@gmail.com>
2022-08-20 20:56:27 +0300

    some string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     let x = 123;
+    let some_string = String::from("hello world worktree");
+    let mut splits = some_string.split(' ');
+
+    splits.next();
+
     println!("Hello, from worktree!");
 }
